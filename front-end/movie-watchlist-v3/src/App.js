fc6373801ec16613a864ef0647a62005576db833
JJW <jonathanwubrave@gmail.com>
2021-10-24 17:31:10 -0400

    add routing

--- front-end/movie-watchlist-v3/src/App.js
+++ front-end/movie-watchlist-v3/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ListMovieComp from "./components/ListMovieComp";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React from "react";
 
 function App() {
   const theme = createTheme({
@@ -12,15 +14,22 @@ function App() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Header />
-        <div>
-          <ListMovieComp />
-        </div>
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <div>
+            <Header />
+            <div>
+              <Switch>
+                <Route path="/" component = {ListMovieComp}></Route>
+                <Route path="/movies" component = {ListMovieComp}></Route>
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </Router>
+    </div>
   );
 }
 
